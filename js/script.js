2cd51b99113c33dc14db63dd8d15575397902464
roy83 <roy83@wp.pl>
2015-06-21 20:28:10 +0200

    js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,34 @@
 (function(){
 
 	function openModal(){
-		$( '.modal' ).addClass( 'isOpen' );
+		$( '.modal' ).addClass( 'isOpen' ).delay( 200 ).queue(function(){
+			$(this)
+				.css({
+					'opacity': 1,
+					'transition': 'opacity 500ms ease-in-out'
+				})
+				.find('.box')
+				.css({
+					'margin': '-160px 0 0 -300px',
+					'transition': 'margin 500ms ease-in-out'
+				})
+				.dequeue();
+		});
 	}
 
 	function closeModal(){
-		$( '.modal' ).removeClass( 'isOpen' );
+		$( '.modal' ).css({
+				'opacity': 0,
+				'transition': 'opacity 1000ms ease-in-out'
+			})
+			.find('.box')
+			.css({
+				'margin': '-460px 0 0 -300px',
+				'transition': 'margin 1000ms ease-in-out'
+			})
+			.delay( 1000 ).queue(function(){
+				$( '.modal' ).removeClass( 'isOpen' ).dequeue();
+			});
 	}
 	
 	$( '.wrapper' ).on( 'click', 'button', openModal );
