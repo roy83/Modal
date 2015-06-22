4ed256457283375c69b9a6ec0b0dd7fdc51e1881
roy83 <roy83@wp.pl>
2015-06-22 13:07:37 +0200

    js, rwd

--- js/script.js
+++ js/script.js
@@ -1,41 +1,44 @@
 (function(){
 
+	var modal = $('.modal'),
+		box = $('.box'),
+		ww, marginValueForOpen, marginValueForClose;
+
+	setValue();
+	$( window ).on( 'resize', setValue );
+
+	function setValue(){
+		ww = $(window).width();
+		marginValueForOpen = ( ww < 670 ) ? {'margin': 0} : {'margin': '-160px 0 0 -300px'},
+		marginValueForClose = ( ww < 670 ) ? {'margin': 0} : {'margin': '-460px 0 0 -300px'};
+		if ( modal.hasClass('isOpen') ) {
+			box.css('margin', ( ww < 670 ) ? 0 : '-160px 0 0 -300px');
+		}
+	}
+
 	function openModal(){
-		$( '.modal' ).addClass( 'isOpen' ).delay( 200 ).queue(function(){
-			$(this)
-				.css({
-					'opacity': 1,
-					'transition': 'opacity 500ms ease-in-out'
-				})
-				.find('.box')
-				.css({
-					'margin': '-160px 0 0 -300px',
-					'transition': 'margin 500ms ease-in-out'
-				})
-				.dequeue();
-		});
+		modal.addClass( 'isOpen' )
+			.delay( 200 ).queue(function(){
+				$(this).css({
+						'opacity': 1,
+				}).find(box).css(marginValueForOpen).dequeue();
+			});
 	}
 
 	function closeModal(){
-		$( '.modal' ).css({
-				'opacity': 0,
-				'transition': 'opacity 1000ms ease-in-out'
-			})
-			.find('.box')
-			.css({
-				'margin': '-460px 0 0 -300px',
-				'transition': 'margin 1000ms ease-in-out'
-			})
-			.delay( 1000 ).queue(function(){
-				$( '.modal' ).removeClass( 'isOpen' ).dequeue();
-			});
+		modal.css({
+			'opacity': 0,
+		}).find(box).css(marginValueForClose)
+		.delay( 500 ).queue(function(){
+			modal.removeClass( 'isOpen' ).dequeue();
+		});
 	}
 	
 	$( '.wrapper' ).on( 'click', 'button', openModal );
 
 	$( '.close' ).on( 'click', closeModal );
 
-	$( '.modal' ).on( 'click', function( e ) {
+	modal.on( 'click', function( e ) {
 		if ( e.target === this ) closeModal();
 	});
 	
